ca00910eae8aeb8901ae6bfebe95b5e2aa8fc679
miha11-hash <d_mihaela@msn.com>
2024-07-06 00:35:32 +0300

    NavLinks component

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,10 +1,19 @@
 
+import Link from "next/link";
+const links = [
+  {href: "/chat", label: "Chat"},
+  {href: "/tours", label: "Tours"},
+  {href: "/tours/new-tour", label: "New Tours"},
+  {href: "/profile", label: "Profile"}
+];
 
 function NavLinks() {
   return (
-    <div>
-      NavLinks
-    </div>
+    <ul className="menu text-base-content mt-4">
+        {links.map((link) => (
+          <li key={link.href}><Link href={link.href} className="text-lg">{link.label}</Link></li>
+        ))}
+    </ul>
   )
 }
 
