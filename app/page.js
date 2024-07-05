1d6b3fb92ca17b16915cd8d83e23903e1d00bb36
miha11-hash <d_mihaela@msn.com>
2024-07-05 15:20:16 +0300

    Home Page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,22 @@
-
+import Link from "next/link";
 
 function HomePage() {
   return (
-    <div>
-      HELLO
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-6xl text-primary font-bold">GPTGenius</h1>
+          <p className="py-6 leading-loose text-lg">
+            GPTGenius: Your AI language companion. Powered by OpenAI, it
+            enhances your conversations, content creation, and more!
+          </p>
+          <Link href="/chat" className="btn btn-secondary">
+            <button>Get Started</button>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomePage
