4f5cb1d2434d329c2f56b55d998be7c17c3ad037
miha11-hash <d_mihaela@msn.com>
2024-07-06 15:09:06 +0300

    Member Profile component

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,11 +1,24 @@
 
 
-function MemberProfile() {
+import { currentUser } from "@clerk/nextjs/server";
+import { UserButton } from "@clerk/nextjs";
+
+async function MemberProfile() {
+    const user= await currentUser();
+    const userEmail=user.emailAddresses[0].emailAddress;
+    
+
   return (
-    <div>
-      Member profile
+    <div className="pl-4">
+      <div className="flex align-center">
+        <UserButton afterSignOutUrl="/"/>
+        <h3 className="ml-4">
+          {user.firstName} {user.lastName}
+        </h3>
+      </div>
+      <h6 className="text-xs">{userEmail}</h6>
     </div>
-  )
+  );
 }
 
 export default MemberProfile
