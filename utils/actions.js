374d6c7ee4d9a720d5c7e18a0e2063e593a78310
miha11-hash <d_mihaela@msn.com>
2024-07-07 20:50:20 +0300

    openai - first response

--- utils/actions.js
+++ utils/actions.js
@@ -1,5 +1,21 @@
 "use server";
 
-export const generateChatResponse = async (chatMessage) => {
-    console.log(chatMessage);
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+export const generateChatResponse = async (message) => {
+    const completion = await openai.chat.completions.create({
+         model: "gpt-3.5-turbo",
+         messages: [
+            {role: "system", content: "You are a helpful assistant..."},
+            {role: "user", content: message},
+         ],
+         temperature: 0,
+      max_tokens: 100,
+    })
+    console.log(completion.choices[0].message);
+    console.log(completion);
 }
