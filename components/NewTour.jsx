3679ec37ade2d37eb9b0cf95650842681e0cab94
miha11-hash <d_mihaela@msn.com>
2024-07-09 01:05:56 +0300

    TourInfo

--- components/NewTour.jsx
+++ components/NewTour.jsx
@@ -33,7 +33,7 @@ const {mutate, isPending, data: tour} = useMutation({
   return (
     <>
       <form onSubmit={handleSubmit} className="max-w-2xl">
-        <h2 className=" mb-4">Select your dream destination</h2>
+        <h2 className=" mb-4 font-semibold">Select your dream destination!</h2>
         <div className="join w-full">
           <input
             type="text"
