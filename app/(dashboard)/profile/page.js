32efc90ea401f9a1d2e05978924adf3c76f59213
miha11-hash <d_mihaela@msn.com>
2024-07-11 20:58:35 +0300

    display tokens

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,10 +1,20 @@
+import { getUserTokensById } from "../../../utils/actions"
 import { UserProfile } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 
+const ProfilePage = async () => {
+  const { userId } = auth();
+  const currentTokens = await getUserTokensById(userId);
 
-const ProfilePage = () => {
-     return  <UserProfile />
-
-  
+  return (
+    <div>
+      <h2 className='mb-8 ml-8 text-xl font-extrabold'>
+        Token Amount : {currentTokens}
+      </h2>
+      <UserProfile routing='hash' />
+    </div>
+  );
 };
 export default ProfilePage;
+
 
--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,11 +1,14 @@
 
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser, auth } from "@clerk/nextjs/server";
 import { UserButton } from "@clerk/nextjs";
+import { getOrGenerateTokens } from "../utils/actions";
 
 async function MemberProfile() {
     const user= await currentUser();
     const userEmail=user.emailAddresses[0].emailAddress;
-    
+    const {userId}=auth();
+    await getOrGenerateTokens(userId);
+  
 
   return (
     <div className="pl-4">
